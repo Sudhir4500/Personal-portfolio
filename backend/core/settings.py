911773130ce34267326c0cf06d7e9d0ff8218f0b
sudhir4500 <sudhirsharma4500@gmail.com>
2025-01-14 08:03:44 +0545

    final5

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost','*']
 
 
 # Application definition
